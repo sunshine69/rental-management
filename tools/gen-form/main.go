545bc704748dd4016100a23cd5e1ae0ecd25916a
steve kieu <steve.kieu@sonichealthcare.com.au>
2024-11-04 13:04:08 +1000

    Use jinja2 to template the go template
    tired of go template automatically escape my html element even outside
    of the rendered area and even try to auto do / insert stupid things

--- tools/gen-form/main.go
+++ tools/gen-form/main.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	ag "github.com/sunshine69/automation-go/lib"
+	u "github.com/sunshine69/golang-tools/utils"
 	"github.com/sunshine69/rental-management/model"
 )
 
@@ -25,10 +26,11 @@ func GetTemplateData() (data map[string]any) {
 func FormGen(structType any, writeDirectory string) {
 	sInfo := ag.ReflectStruct(structType, `([^\:\s]+):"([^"]+)"`)
 	destFile := writeDirectory + "/" + sInfo.Name + ".html"
-
+	fList := []string{}
 	fieldProp := map[string]map[string]any{}
 	for _, v := range sInfo.FieldName {
-		fieldProp[v] = map[string]any{"display": true, "ele": "<input", "close_ele": "/>", "type": "text", "label": ""}
+		fList = append(fList, v)
+		fieldProp[v] = map[string]any{"display": true, "ele": "input", "close_ele": "/>", "type": "text", "label": ""}
 		tags := sInfo.TagCapture[v]
 		for _, tagset := range tags {
 			if (tagset[1] == "form" && len(tagset) >= 3 && tagset[2] == "-") || v == "Id" {
@@ -38,27 +40,31 @@ func FormGen(structType any, writeDirectory string) {
 				fieldProp[v]["label"] = "*"
 			}
 			if tagset[1] == "form" && len(tagset) >= 3 && strings.Contains(tagset[2], "ele=textarea") {
-				fieldProp[v]["ele"] = "<textarea"
-				fieldProp[v]["close_ele"] = "></textarea>"
+				fieldProp[v]["ele"] = "textarea"
+				fieldProp[v]["close_ele"] = ""
 			}
 		}
 	}
-	// fmt.Printf("%v\n", u.JsonDump(fieldProp, ""))
+	fmt.Printf("DEBUG: %s\n", u.JsonDump(fieldProp, ""))
+
+	fmt.Printf("DEBUG FLIST: %s\n", u.JsonDump(fList, ""))
 	data := map[string]any{
 		"formName":   sInfo.Name,
 		"formClass":  "form-group",
 		"formAction": "/" + strings.ToLower(sInfo.Name),
 		"formID":     strings.ToLower(sInfo.Name),
-		"fInfo":      sInfo,
+		"fList":      fList,
 		"fieldProp":  fieldProp,
 	}
-	ag.GoTemplateFile("tools/gen-form/form.go.tmpl", destFile, data, 0640)
+	// ag.GoTemplateFile("tools/gen-form/form.go.tmpl", destFile, data, 0640)
+	ag.TemplateFile("tools/gen-form/form.jinja2", destFile, data, 0640)
 	formNameList := []string{}
 	for _, f := range model.AllModelObjects {
 		sInfo := ag.ReflectStruct(f, `form:"([^"]+)"`)
 		formNameList = append(formNameList, sInfo.Name)
 	}
-	ag.GoTemplateFile("tools/gen-form/form-header.go.tmpl", writeDirectory+"/form-header.html", map[string]any{"formNameList": formNameList}, 0640)
+	// ag.GoTemplateFile("tools/gen-form/form-header.go.tmpl", writeDirectory+"/form-header.html", map[string]any{"formNameList": formNameList}, 0640)
+	ag.TemplateFile("tools/gen-form/form-header.go.tmpl", writeDirectory+"/form-header.html", map[string]any{"formNameList": formNameList}, 0640)
 	// Generate some common func handler and form validation to copy/paste into the app.go
 }
 
